3777b30af769c35275a55f9bdfc9f6462eefa409
Paulo Correa <henriqueph@hotmail.com>
2020-11-25 22:42:24 -0300

    Initial commit, with base spring boot files.

---
+++ src/test/java/br/com/phmr/springbootecomm/SpringBootEcommApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.phmr.springbootecomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcommApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
