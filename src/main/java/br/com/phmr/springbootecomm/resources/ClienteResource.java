517dda6ab3e29c26ffa363301be4c83c069846f2
Paulo H. Corrêa <henriqueph@hotmail.com>
2021-02-12 19:31:56 -0300

    Retomada do curso. Objs Cliente

--- src/main/java/br/com/phmr/springbootecomm/resources/ClienteResource.java
+++ src/main/java/br/com/phmr/springbootecomm/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package br.com.phmr.springbootecomm.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.phmr.springbootecomm.domain.Cliente;
 import br.com.phmr.springbootecomm.dto.ClienteDTO;
+import br.com.phmr.springbootecomm.dto.ClienteNewDTO;
 import br.com.phmr.springbootecomm.resources.exceptions.StandardError;
 import br.com.phmr.springbootecomm.services.ClienteService;
 
@@ -34,6 +37,13 @@ public class ClienteResource {
 			return ResponseEntity.status(404).body(new StandardError("Cliente não encontrado. Id:" + id));
 		}
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDTO) {
+		var newObj = service.insert(service.fromDTO(objDTO));
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
--- src/main/java/br/com/phmr/springbootecomm/services/ClienteService.java
+++ src/main/java/br/com/phmr/springbootecomm/services/ClienteService.java
@@ -11,19 +11,33 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import br.com.phmr.springbootecomm.domain.Cidade;
 import br.com.phmr.springbootecomm.domain.Cliente;
+import br.com.phmr.springbootecomm.domain.Endereco;
+import br.com.phmr.springbootecomm.domain.enums.TipoCliente;
 import br.com.phmr.springbootecomm.dto.ClienteDTO;
+import br.com.phmr.springbootecomm.dto.ClienteNewDTO;
 import br.com.phmr.springbootecomm.repositories.ClienteRepository;
+import br.com.phmr.springbootecomm.repositories.EnderecoRepository;
 
 @Service
 public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElse(null);
+	}
+
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return repo.save(obj);
 	}
 
 	public Cliente update(Cliente obj) {
@@ -54,6 +68,22 @@ public class ClienteService {
 
 	public Cliente fromDTO(ClienteDTO obj) {
 		return new Cliente(obj.getId(), obj.getNome(), obj.getEmail());
+	}
+
+	public Cliente fromDTO(ClienteNewDTO obj) {
+		Cliente cli = new Cliente(null, obj.getNome(), obj.getEmail(), obj.getCpfOuCnpj(),
+				TipoCliente.toEnum(obj.getTipo()));
+		cli.getTelefones().add(obj.getTelefone1());
+		if (obj.getTelefone2() != null) {
+			cli.getTelefones().add(obj.getTelefone2());
+		}
+		if (obj.getTelefone3() != null) {
+			cli.getTelefones().add(obj.getTelefone3());
+		}
+		Cidade cid = new Cidade(obj.getCidadeId(),null, null);
+		new Endereco(null, obj.getLogradouro(), obj.getNumero(), obj.getComplemento(), 
+				obj.getBairro(), obj.getCep(), cli, cid);
+		return cli;
 	}
 
 }
